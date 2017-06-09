596f087e692882abe6dfedb7b5c9f4dde38bfd16
Larry <larryjamestaylor@gmail.com>
2017-06-09 15:54:31 -0700

    finish js, mostly

--- js/scripts.js
+++ js/scripts.js
@@ -7,36 +7,31 @@ var pingPong = function(userInput){
   for (var index = 1; index <= userInput; index += 1) {
     if (index % 15 === 0) {
       numberArray.push("ping-pong")
-      // console.log("ping-pong")
-    }
-    else if (index % 5 === 0) {
+      console.log("ping-pong")
+    } else if (index % 5 === 0) {
       numberArray.push("pong")
-      // console.log("pong")
-     }
-    else if (index % 3 === 0){
+      console.log("pong")
+     } else if (index % 3 === 0){
       numberArray.push("ping")
-      // console.log("ping")
-    }
-    else {
+      console.log("ping")
+    } else {
       numberArray.push(index)
     }
-  }
-    return pingPong;
+  } return pingPong;
 };
 
-
-// Front end results here
-var display = function(output) {
-  $("#result ul").empty();
-  output.forEach(function(changed){
-  $("#result ul").append("<li>" + changed  + "</li>");
-  });
-};
 // Front end logic here
 $(document).ready(function(){
   $("form.inputForm").submit(function(event){
     var userInput = parseInt($("input#inputNumber").val());
     console.log(userInput);
-    event.preventDefault();
+
+// Front end results here
+var display = pingPong(userInput)
+  $("#result ul").empty();
+  display.forEach(function(changed){
+  $("#result ul").append("<li>" + changed  + "</li>");
   });
+  event.preventDefault();
+});
 });
